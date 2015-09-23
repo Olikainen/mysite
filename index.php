3df5ad23bb0289a9368da8d99d1bf03e7c0a64d3
pupil <pupil@debian>
2015-09-23 16:45:36 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Список всех записей в таблице</h1>
+		<ol>
+			<li>
+				<?php echo "сюда помещаем записи"; ?>
+			</li>
+		</ol>
+			
+		</u>
+</body>
+</html>
